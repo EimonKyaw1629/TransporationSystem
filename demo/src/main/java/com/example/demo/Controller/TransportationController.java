694b7b8ca851d9f17bcf630004b7b59891f354ff
kimwoonyong <dnsdyd200@gmail.com>
2018-11-05 17:37:04 +0900

    insert

--- demo/src/main/java/com/example/demo/Controller/TransportationController.java
+++ demo/src/main/java/com/example/demo/Controller/TransportationController.java
@@ -4,21 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.demo.DAO.TransportationDAO;
 import com.example.demo.Service.TransportationService;
 import com.example.demo.model.DutyPersonInfo;
 import com.example.demo.model.FareInfo;
+
 
 @Controller
 public class TransportationController {
 
 	@Autowired
 	private TransportationService tService;
+	
+	@Autowired
+	private TransportationDAO dao;
 	
 	public ModelAndView getLoginInfo(String vname)
 	{
@@ -27,24 +30,6 @@ public class TransportationController {
 	    modelAndView.setViewName(vname);
 	    return modelAndView;
 	}
-	
-	//@RequestMapping(value="/search/starteki= {starteki}&endeki= {endeki}")
-	/*
-	public ModelAndView deletePersonInfo(@PathVariable String starteki, @PathVariable String endeki,Model m) throws IOException {
-	try
-	{
-		String cost=	tService.getFare(starteki, endeki);
-		 ModelAndView modelAndView = this.getLoginInfo("FrmTransportation");
-		
-		return modelAndView;
-	}
-	catch(Exception e)
-	{
-		//return modelAndView;
-	}
-		
-	}
-	*/
 
 	@RequestMapping(value={ "/insert" }, method = RequestMethod.POST)//Register
 	public String index(@ModelAttribute FareInfo info, @ModelAttribute DutyPersonInfo dutyPersonInfo, Model m)
@@ -52,6 +37,9 @@ public class TransportationController {
 		
 		System.out.println(dutyPersonInfo);
 		System.out.println(info);
+		
+		dao.insert(info);
+		
 		return "TransportationList";
 	}
 	
--- demo/src/main/java/com/example/demo/DAO/TransportationDAO.java
+++ demo/src/main/java/com/example/demo/DAO/TransportationDAO.java
@@ -1,6 +1,5 @@
 package com.example.demo.DAO;
 
-import java.awt.print.Book;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -17,24 +16,35 @@ public class TransportationDAO {
 	@Autowired
     private JdbcTemplate jdbcTemplate;
 	
-	/*public List<FareInfo> selectBookList() {
-        return jdbcTemplate.query("SELECT * FROM book", new RowMapper<FareInfo>() {
+	public List<FareInfo> selectBookList() {
+        return jdbcTemplate.query("SELECT * FROM Tb_DutyPerson", new RowMapper<FareInfo>() {
             public FareInfo mapRow(ResultSet rs, int rowNum) throws SQLException {
             	FareInfo fareInfo = new FareInfo();
+            	
             	fareInfo.setFareID(rs.getInt(1));
             	fareInfo.setPersonID(rs.getInt(2));
             	fareInfo.setUseDate(rs.getString(3));
             	fareInfo.setDepartureStation(rs.getString(4));
-                return book;
-                
-            	private int FareID;
-            	private int PersonID;
-            	private String UseDate;
-            	private String Departure_Station;
-            	private String Arrival_Station;
-            	private String Purpose;
-            	private int Fare;
+            	fareInfo.setArrivalStation(rs.getString(5));;
+            	fareInfo.setPurpose(rs.getString(6));
+            	fareInfo.setFare(rs.getInt(7));
+                return fareInfo;
             }
         });
-    }*/
+    }
+    
+	public void insert (FareInfo fareInfo) {
+	     jdbcTemplate.update ("INSERT INTO Tb_Fare (PersonID, UseDate, Departure_station, Arrival_station, Purpose, Fare) VALUES (?,?,?,?,?,?)" ,
+	             1, fareInfo.getUseDate(), fareInfo.getDepartureStation(), fareInfo.getArrivalStation(), fareInfo.getPurpose(), fareInfo.getFare());
+	}
+
+	public void delete (FareInfo fareInfo) {
+	     jdbcTemplate.update ( "DELETE FROM book WHERE book_id =?" ,
+	    		 fareInfo.getFareID());
+	}
+	
+	/*public  void update (FareInfo fareInfo) {
+	     jdbcTemplate.update ( "UPDATE Tb_Fare SET UseDate = ?, Departure_station =? WHERE PersonID =?" ,
+	     );
+	}*/
 }
