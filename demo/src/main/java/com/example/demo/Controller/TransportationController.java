ab425b812068d47eda27b97ff19557e2ba873942
kimwoonyong <dnsdyd200@gmail.com>
2018-11-05 15:40:21 +0900

    commit

--- demo/src/main/java/com/example/demo/Controller/TransportationController.java
+++ demo/src/main/java/com/example/demo/Controller/TransportationController.java
@@ -1,15 +1,16 @@
 package com.example.demo.Controller;
 
-import java.io.IOException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.Service.TransportationService;
+import com.example.demo.model.DutyPersonInfo;
+import com.example.demo.model.FareInfo;
 
 @Controller
 public class TransportationController {
@@ -43,10 +44,17 @@ public class TransportationController {
 	}
 	*/
 	
-	@RequestMapping(value="/Register")//Register
-	
-	public String index()
+	@RequestMapping(value={ "/", "/insertTransit" })//Register
+	public String index(@PathVariable DutyPersonInfo dutyPersonInfo, @PathVariable FareInfo fareInfo, Model m)
 	{
-		return "Register";
+		return "FrmTransportation";
 	}
+	
+	@RequestMapping(value="/TransportationList",  method = RequestMethod.GET)//Register
+	public String getListPage()
+	{
+		
+	    return "TransportationList";
+	}
+	
 }
